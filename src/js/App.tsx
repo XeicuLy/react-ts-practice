165a85e003711644c54f6361cbcb9da07ef90a12
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-28 17:32:05 +0900

    :sparkles: feat: 基本的な処理をTSを用いて記述しました。

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,5 +1,18 @@
+import { Practice1 } from '../practices/Practice1';
+import { Practice2 } from '../practices/Practice2';
+import { Practice3 } from '../practices/Practice3';
+
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <Practice1 />
+      <hr />
+      <Practice2 />
+      <hr />
+      <Practice3 />
+    </>
+  );
 };
 
 export default App;
---
+++ src/practices/Practice2.tsx
@@ -0,0 +1,15 @@
+export const Practice2 = () => {
+  const onClickPractice = () => {
+    console.log(getTotalFee(1000));
+  };
+  const getTotalFee = (num: number): number => {
+    const total = num * 1.1;
+    return total;
+  };
+  return (
+    <div>
+      <p>練習問題2: 返却値の型指定</p>
+      <button onClick={onClickPractice}>練習問題2を実行</button>
+    </div>
+  );
+};
